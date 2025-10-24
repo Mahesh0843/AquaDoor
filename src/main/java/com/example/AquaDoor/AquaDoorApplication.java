72bc2f0aebcf607cb5154be8c07457fc7a5ac226
Mahesh0843 <singulurimahesh123@gmail.com>
2025-10-24 11:21:40 +0530

    Initial project commit

---
+++ src/main/java/com/example/AquaDoor/AquaDoorApplication.java
@@ -0,0 +1,13 @@
+package com.example.AquaDoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaDoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaDoorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AquaDoor/AquaDoorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AquaDoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaDoorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
