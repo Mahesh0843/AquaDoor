0d4feb43a14d1452d62a9b4837d674ba40080ffb
Surendra Pokala <surendrapokala5@gmail.com>
2025-10-25 17:22:13 +0530

    entites

--- src/main/java/com/example/AquaDoor/controller/AgentController.java
+++ src/main/java/com/example/AquaDoor/controller/AgentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/aquawater/agents")
+@RequestMapping("/aquadoor/agents")
 public class AgentController {
 
     @Autowired
